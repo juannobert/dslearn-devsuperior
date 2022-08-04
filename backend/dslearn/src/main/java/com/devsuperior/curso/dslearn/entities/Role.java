bb3b3874589958459fb1a9605cacb5e52062c311
Juan Nobert <nobert407@gmail.com>
2022-08-04 10:50:39 -0300

    Entity User created

--- backend/dslearn/src/main/java/com/devsuperior/curso/dslearn/entities/Role.java
+++ backend/dslearn/src/main/java/com/devsuperior/curso/dslearn/entities/Role.java
@@ -1,5 +1,6 @@
 package com.devsuperior.curso.dslearn.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -10,7 +11,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
