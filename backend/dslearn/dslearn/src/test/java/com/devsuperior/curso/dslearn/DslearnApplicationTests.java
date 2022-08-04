d1147db5c27688dd6d67bd680e9dc82af97a435a
Juan Nobert <nobert407@gmail.com>
2022-08-04 10:25:26 -0300

    Backend created

---
+++ backend/dslearn/dslearn/src/test/java/com/devsuperior/curso/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.curso.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
