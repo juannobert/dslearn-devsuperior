90608ff6d84555f7915912f5c5b31b0a72af0bd3
Juan Nobert <nobert407@gmail.com>
2022-08-09 10:05:02 -0300

    OAuth custom error and authorizations exceptions implemented

--- backend/dslearn/src/main/java/com/devsuperior/curso/dslearn/resources/exceptions/ResourceExceptionHandler.java
+++ backend/dslearn/src/main/java/com/devsuperior/curso/dslearn/resources/exceptions/ResourceExceptionHandler.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.devsuperior.curso.dslearn.services.exceptions.DataBaseException;
+import com.devsuperior.curso.dslearn.services.exceptions.ForbiddenException;
 import com.devsuperior.curso.dslearn.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.curso.dslearn.services.exceptions.UnauthorizedException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -56,6 +58,19 @@ public class ResourceExceptionHandler {
 			err.add(field.getField(), field.getDefaultMessage());
 		}
 		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+	@ExceptionHandler(ForbiddenException.class)
+	public ResponseEntity<OAuthCustomError> forbidden(ForbiddenException e,HttpServletRequest request) {
+		var err = new OAuthCustomError("Forbidden",e.getMessage());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+		
+	}
+	@ExceptionHandler(UnauthorizedException.class)
+	public ResponseEntity<OAuthCustomError> unauthorized(UnauthorizedException e,HttpServletRequest request) {
+		var err = new OAuthCustomError("Unauthorized",e.getMessage());
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(err);
 		
 	}
 	
